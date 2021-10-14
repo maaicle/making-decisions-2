e76736d17341c4bcbcda913aef0d8de724d7cb21
Michael Hardcastle <maaicle@gmail.com>
2021-10-14 16:19:04 -0600

    all problems completed

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -173,28 +173,32 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 */
 
 //Code Here
-switch(letterGrade) {
-  case 'A':
-    console.log('The student is doing excellently.')
-    break;
-  case 'B':
-    console.log('The student is doing well.')
-    break;
-  case 'C':
-    console.log('The student is doing alright.')
-    break;
-  case 'D':
-    console.log('The student is not doing very well.')
-    break;
-  case 'F':
-    console.log('The student is failing.')
-    break;  
-};
+// switch(letterGrade) {
+//   case 'A':
+//     console.log('The student is doing excellently.')
+//     break;
+//   case 'B':
+//     console.log('The student is doing well.')
+//     break;
+//   case 'C':
+//     console.log('The student is doing alright.')
+//     break;
+//   case 'D':
+//     console.log('The student is not doing very well.')
+//     break;
+//   case 'F':
+//     console.log('The student is failing.')
+//     break;  
+// };
 
 
 ////////// PROBLEM 11 //////////
 /* The famous FizzBuzz, Devmountain style!
-  Create a for loop that iterates from 1 to 100. In this for loop, using some conditional logic, if the number your for loop is currently on is divisible by 3, console.log 'Dev'. If the number is divisible by 5, console.log 'mountain'. If the number is divisible by 5 & 3, console.log 'Devmountain'. If the number is not divisible by 5 or 3, console.log the number itself. Hint: Look up the modulo operator.
+  Create a for loop that iterates from 1 to 100. 
+  In this for loop, using some conditional logic, if the number your for loop is currently on is divisible by 3, 
+  console.log 'Dev'. If the number is divisible by 5, console.log 'mountain'. If the number is divisible by 5 & 3, 
+  console.log 'Devmountain'. If the number is not divisible by 5 or 3, console.log the number itself. 
+  Hint: Look up the modulo operator.
 
   Your output should look like:
   1
@@ -217,3 +221,14 @@ switch(letterGrade) {
 */
 
 //Code Here
+for (let i = 1; i <= 100; i++) {
+  if (i % 15 === 0) {  
+    console.log("Devmountain")
+  } else if(i % 3 === 0) {
+    console.log("Dev")
+  } else if (i % 5 === 0) {
+    console.log("Mountain")
+  } else {
+    console.log(i)
+  }
+};
